a1778e357b0b12ea95e909bb19f1ae3d4f1ae5d6
Noa-Denise <noadenise.ishac@qlok.se>
2025-06-18 12:30:06 +0200

    Added DTOs for Invoice and item details

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -41,7 +41,6 @@ namespace InvoiceSystemAPI.Controllers
             };
 
             return Ok(response);
-
         }
     }
 }
---
+++ DTOs/InvoiceItemDetailsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace InvoiceSystemAPI.DTOs
+{
+    public class InvoiceItemDetailsDTO
+    {
+        public string Description { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Total { get; set; }
+    }
+}
